5cc4efb7099b47e90183c1cfcc2c9cdd8c0919ce
yohannes <yohannesg218@gmail.com>
2026-03-17 14:25:21 -0700

    ....

--- src/users/dto/register.dto.ts
+++ src/users/dto/register.dto.ts
@@ -1,6 +1,8 @@
 import { IsEmail ,IsString,MinLength,IsStrongPassword,Matches } from "class-validator";
 
 export class RegisterDto {
+    @IsString()
+    username: string;
     @IsEmail()
     email: string;
     @IsString()
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,14 +1,72 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Users } from './Schema/user.schema';
 import { Model } from 'mongoose';
 import { JwtService } from '@nestjs/jwt';
 import { EmailService } from '../email/email.service';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
+import * as bcrypt from 'bcryptjs';
+
 @Injectable()
 export class UsersService {
   constructor(
-    @InjectModel(Users.name) private userModel: Model<Users>,
-    private jwtService: JwtService,
-    private emailService: EmailService,
+    @InjectModel(Users.name) private readonly userModel: Model<Users>,
+    private readonly jwtService: JwtService,
+    private readonly emailService: EmailService,
   ) {}
+
+  async register(registerData: RegisterDto) {
+    const { username, email, password } = registerData;
+    const exists = await this.userModel.exists({ email });
+    if (exists) {
+      throw new BadRequestException('Email already in use');
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new this.userModel({
+      username,
+      email,
+      password: hashedPassword,
+      isVerified: false,
+    });
+
+    await newUser.save();
+
+    const token = this.jwtService.sign(
+      { sub: newUser._id.toString() }, 
+      { expiresIn: '1d' },
+    );
+
+    return {
+      message: 'Registration successful',
+    };
+  }
+
+  async login(loginData: LoginDto) {
+    const { email, password } = loginData;
+
+    const user = await this.userModel.findOne({ email }).select('+password');
+
+    if (!user) {
+      throw new BadRequestException('Invalid credentials');
+    }
+
+    if (!user.isVerified) {
+      throw new BadRequestException('Email not verified');
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      throw new BadRequestException('Invalid credentials');
+    }
+    const token = this.jwtService.sign(
+      { sub: user._id.toString() }, 
+    );
+
+    return {
+      message: 'Login successful',
+      token,
+    };
+  }
 }
