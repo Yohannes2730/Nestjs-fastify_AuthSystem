a38cd79496a9d6ed50f15ed20ee1c727a063c961
yohannes <yohannesg218@gmail.com>
2026-03-17 01:23:50 -0700

    ..

--- src/users/Schema/user.schema.ts
+++ src/users/Schema/user.schema.ts
@@ -1,1 +1,18 @@
-export class User {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Users extends Document {
+  @Prop({ required: true, unique: true })
+  username: string;
+  @Prop({ required: true, unique: true })
+  email: string;
+  @Prop({ required: true })
+  password: string;
+  @Prop({ default: false })
+  isVerified: boolean;
+  @Prop({ default: Date.now() })
+  createdAt: Date;
+}
+
+export const userSchema = SchemaFactory.createForClass(Users);
