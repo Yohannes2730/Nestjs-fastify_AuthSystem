52ce6829c07fa3a0a5d77a043124cc00350fc9fc
yohannes <yohannesg218@gmail.com>
2026-03-27 02:58:46 -0700

    ....

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,44 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmailModule } from './email/email.module';
 import { UsersModule } from './users/users.module';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { MongooseModule } from '@nestjs/mongoose';
+import config from './config/config';
 @Module({
-  imports: [EmailModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [config],
+    }),
+
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      global: true,
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('jwt.secret'),
+        signOptions: { expiresIn: '1h' },
+      }),
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const uri = configService.get<string>('database.connectionString');
+        if (!uri) {
+          throw new Error('MongoDB connection string is not defined!');
+        }
+        return {
+          uri
+      };
+      },
+    }),
+
+    UsersModule,
+
+    EmailModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { EmailModule } from 'src/email/email.module';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { Users, userSchema } from './Schema/user.schema';
 @Module({
-  imports: [EmailModule],
+  imports: [
+    MongooseModule.forFeature([
+      { name: Users.name, schema: userSchema },
+    ]),
+        EmailModule, 
+
+  ],
+  
   controllers: [UsersController],
   providers: [UsersService],
 })
