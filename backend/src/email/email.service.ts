2168590920d7e251563426688d9715c82cb896ab
yohannes <yohannesg218@gmail.com>
2026-05-05 13:16:27 -0700

    ...

--- backend/src/email/email.service.ts
+++ backend/src/email/email.service.ts
@@ -10,7 +10,6 @@ import { MailerService } from '@nestjs-modules/mailer';
 @Injectable()
 export class EmailService {
   constructor(
-    private readonly mailerService: MailerService,
     @InjectModel(Users.name) private readonly userModel: Model<Users>,
     @InjectModel(EmailOtp.name) private readonly otpModel: Model<EmailOtp>,
   ) {}
